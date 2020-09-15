4b363084c4f77635d3fdda5a8ca859afc58ff2f4
FrozenHeart247 <kristalov2011@gmail.com>
2020-09-15 08:49:05 +0300

    New project

---
+++ BookSportStoreExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookSportStoreExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
